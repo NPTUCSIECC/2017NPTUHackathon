aabdff7047909666202a982bd242bf989a340349
Vagrant <xup6u06m4@gmail.com>
2017-05-07 12:10:55 +0800

    v1.0

--- app.js
+++ app.js
@@ -13,12 +13,54 @@ app.get('/',function(req,res){
 })
 
 app.get('/pageinfo',function(req,res){
-  res.render('views/singlepage', {
-
+  fs.readFile('list.json', 'utf8', function (err, data) {
+    data = JSON.parse(data)
+    for(let item of data) {
+      if(item.title == req.query.title) {
+        // res.render('singlepage', {
+          // title: item.title,
+          // info: item.info,
+        // })
+  let pm25Url = 'http://opendata.epa.gov.tw/ws/Data/ATM00625/?%24skip=0&%24top=1000&format=json';
+  let waether = 'http://opendata.epa.gov.tw/ws/Data/ATM00698/?%24skip=0&%24top=1000&format=json';
+  request(pm25Url, (error, response, body) => {
+    var pm25, weather;
+    let data = JSON.parse(body);
+    for(let item of data) {
+      if(item.Site === '屏東') {
+        pm25 = item.PM25;
+        break;
+      }
+    }
+    request(waether, (error, response, body) => {
+      let data = JSON.parse(body);
+      for(let item of data) {
+        if(item.SiteName === '恆春' && item.Weather != "") {
+          weather = item.Weather;
+          moisture = item.Moisture;
+          break;
+        }
+      }
+      // res.json({
+        // pm25,
+        // weather,
+        // moisture
+      // });
+        res.render('singlepage', {
+          title: item.title,
+          info: item.info,
+          pm25: pm25,
+          moisture: moisture,
+          img: item.img
+        })
+    });
+  });
+      }
+    }
   })
 })
 
-app.get('/test', function (req, res) {
+app.get('/api', function (req, res) {
   let pm25Url = 'http://opendata.epa.gov.tw/ws/Data/ATM00625/?%24skip=0&%24top=1000&format=json';
   let waether = 'http://opendata.epa.gov.tw/ws/Data/ATM00698/?%24skip=0&%24top=1000&format=json';
   request(pm25Url, (error, response, body) => {
